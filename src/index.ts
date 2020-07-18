4a880cedf155565b261475f2d74475e46be659c7
khashayar <khashayar.k.m@gmail.com>
2020-07-18 18:15:31 +0430

    migrated to typescript

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+// Setup
+import express = require('express');
+var app = express();
+import bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+import ejs = require('ejs');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+// posts = [];
+
+
+// Routes
+app.get("/", (req, res) => {
+    getAllPosts().then(posts => {
+        res.render('index', { posts: posts });
+    }).catch(err => {
+        res.status(400).send("Unable to save data");
+    });
+});
+
+app.post('/addpost', (req, res) => {
+    addPost(req.body).then(result => {
+        res.redirect('/');
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send("Unable to save data");
+    });
+});
+
+
+import fs = require('fs');
+
+const { promisify } = require('util');
+const { json } = require('body-parser');
+
+const readdir = promisify(require('fs').readdir)
+
+async function addPost(post) {
+    fs.writeFile(`./data/posts/${Date.now()}`, JSON.stringify(post), function (err) {
+        if (err) return console.log(err);
+    });
+}
+
+
+async function getAllPosts() {
+    let files = await readdir(__dirname + "/../data/posts/");
+    let data = files.map((file) => JSON.parse(fs.readFileSync(__dirname + '/../data/posts/' + file).toString()))
+    return data;
+}
+
+// Listen
+app.listen(3000, () => {
+    console.log('Server listing on 3000');
+})
