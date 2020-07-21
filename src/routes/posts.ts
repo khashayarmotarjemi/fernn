8ea140716329a8bc1923895ac300a42033e2e4d1
khashayar <khashayar.k.m@gmail.com>
2020-07-21 13:09:56 +0430

    used bootstrap and ejs, added about page, seperated upload and posts pages

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -6,10 +6,18 @@ const router = express.Router();
 export function postsRouter(postsRepo : PostsRepo) {
     router.get("/", (req: express.Request, res: express.Response) => {
         postsRepo.getAllPosts().then(posts => {
-            res.render('index', { posts: posts });
+            res.render('pages/index', { posts: posts });
         }).catch(err => {
             res.status(400).send("unable to get data");
         });
+    });
+
+    router.get("/deck", (req: express.Request, res: express.Response) => {
+        res.render('pages/deck');
+    });
+
+    router.get("/about", (req: express.Request, res: express.Response) => {
+        res.render('pages/about');
     });
     
     router.post('/addpost', upload.single("file"),
