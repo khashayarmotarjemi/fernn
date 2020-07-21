c1470876854c56a892cf23cc0d76caed024c601e
khashayar <khashayar.k.m@gmail.com>
2020-07-21 14:15:49 +0430

    basic local plain username password without database and hashing (very unsecure\!) login to access posting with passport.js

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import ejs = require('ejs');
 import {PostsRepo, MulterFile ,upload} from './domain/posts_repo'
 import {postsRouter} from './routes/posts'
 
+import passport = require('passport');
+import passportLocal = require('passport-local');
+
+let LocalStrategy = passportLocal.Strategy;
+
 var app = express();
 
 app.use(bodyParser.json())
@@ -15,11 +20,50 @@ app.set('view engine', 'ejs');
 
 const postsRepo = new PostsRepo();
 const router = postsRouter(postsRepo);
-app.use('/',router);
 
 app.use( express.static( __dirname + "/../public" ) );
+app.use(require('cookie-parser')());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username: string, password:string, done) {
+    if(username == 'khashayar' && password == "12345") {
+        return done(null, {username: 'khashayar'})
+    } else {
+        return done(null, false, { message: 'Incorrect username or password' });
+    }
+    // User.findOne({ username: username }, function (err, user) {
+    //   if (err) { return done(err); }
+    //   if (!user) {
+    //     return done(null, false, { message: 'Incorrect username.' });
+    //   }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+    //   return done(null, user);
+    // });
+  }
+));
+
+
+app.use('/',router);
+
 
 // Listen
 app.listen(4000, () => {
     console.log('Server listing on 3000');
 })
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,9 +1,11 @@
 import express = require('express')
 import { PostsRepo, upload, MulterFile } from '../domain/posts_repo';
 const router = express.Router();
- 
+import passport = require('passport');
 
-export function postsRouter(postsRepo : PostsRepo) {
+import { RequestWithUser } from '../custom_req'
+
+export function postsRouter(postsRepo: PostsRepo) {
     router.get("/", (req: express.Request, res: express.Response) => {
         postsRepo.getAllPosts().then(posts => {
             res.render('pages/index', { posts: posts });
@@ -12,14 +14,26 @@ export function postsRouter(postsRepo : PostsRepo) {
         });
     });
 
-    router.get("/deck", (req: express.Request, res: express.Response) => {
-        res.render('pages/deck');
+    router.get("/deck", (req: RequestWithUser, res: express.Response) => {
+        if (req.user) {
+            res.render('pages/deck');
+        } else {
+            res.render('pages/login')
+        }
     });
+
+    router.post('/login',
+        passport.authenticate('local'),
+        function (req, res) {
+            // If this function gets called, authentication was successful.
+            // `req.user` contains the authenticated user.
+            res.redirect('/deck');
+        });
 
     router.get("/about", (req: express.Request, res: express.Response) => {
         res.render('pages/about');
     });
-    
+
     router.post('/addpost', upload.single("file"),
         (req: express.Request & { file: MulterFile }, res: express.Response) => {
             postsRepo.addPost(req).then(result => {
@@ -28,9 +42,7 @@ export function postsRouter(postsRepo : PostsRepo) {
                 console.log(err);
                 res.status(400).send("Unable to save data");
             });
-    
+
         });
     return router;
 }
-
-
